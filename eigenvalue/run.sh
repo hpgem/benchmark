79bc5f57b99f9d43968b8c2418fc3ad5346b82a2
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2022-07-06 17:12:29 +0200

    update run with cli

--- eigenvalue/run.sh
+++ eigenvalue/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-mpirun -np 1 ~/hpgem_organization/hpgem/build/applications/DG-Max/Programs/DGMaxEigen -m ./meshes/mesh_D2X10N1.hpgem --method DGMAX_PROJECT -e 10 --structure 0 --points 1,1 --order 1 -- eps_ncv 1
+mpirun -np 1 ~/hpgem_organization/hpgem/build/applications/DG-Max/Programs/DGMaxEigen -m ./meshes/mesh_D2X10N1.hpgem --method DGMAX_PROJECT -e 10 --structure 0 --points 0.,0. --order 1 --use_jdmax -- eps_ncv 1
 
