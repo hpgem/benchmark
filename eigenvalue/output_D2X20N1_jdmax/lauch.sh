f6957067b3ad25df3c297ebe9367497c2d9cf6f7
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2022-08-09 11:37:09 +0200

    new results

---
+++ eigenvalue/output_D2X20N1_jdmax/lauch.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+~/hpgem_organization/hpgem/build/applications/DG-Max/Programs/DGMaxEigen --method DGMAX_PROJECT -m ../meshes/mesh_D2X20N1.hpgem  --points 0,0 -e 10 --order 1 --structure ".*,1" -- -eps_ncv 25 -st_ksp_type preonly -st_pc_type lu -st_pc_factor_mat_solver_type mumps
